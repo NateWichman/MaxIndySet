91f4f467cacc66ba9622dcc2ba077af0a218ab0e
Nathan Wichman <wichmann@eos19.cis.gvsu.edu>
2018-11-02 18:39:38 -0400

    Final Commit

--- DominatingSet.java
+++ DominatingSet.java
@@ -48,6 +48,23 @@ public class DominatingSet {
 		for(int j = 0;j < n;j++) {
 		    if (i!=j && adjacencyMatrix[i][j] == 1 && verticesInSet[j] == 1) {
 			verticesCovered[i] = 1;
+
+			/* if Vertex j is in the Set and the adjacent Vertex i is also in
+			 * the Set, then the Set is not independent. Two Vertices in the 
+			 * Set cannot be adjacent to each other for the set to be independent.
+			 * It still could be dominating, but not Independent.
+			 * A Maximal Independent Set is also a dominating set
+			 * in the graph. This is because if there was a Vertex that was
+			 * not adjacent to any of the verticies in the Maximal Independent
+			 * set, then it should be in the Set as seeing adding it wouldn't 
+			 * touch any of the other verticies. Therefore, if every vertex has
+			 * to be adjacent to at least one vertex of the Maximal Independent Set,
+			 * then the MIS is also a Dominating set.*/
+			if(verticesInSet[i] == 1){
+				/* Checking to see if the adjacent vertex is in the set,
+				 * if it is, the set is not Independent. */
+				return false;
+			}
 			break;
 		    }
 		}
@@ -97,7 +114,7 @@ public class DominatingSet {
 	Optional result = LongStream.range(1,twoToN).parallel().
 	    filter(e ->{ return(isDominatingSet(e));}).
 	    mapToObj(e -> new Pair(e,n)).
-	    collect(Collectors.minBy(comparator));
+	    collect(Collectors.maxBy(comparator));
 
 	System.out.println("Result is: "+result.toString());
 
